a50cb617921f5c19e3b216ed34092b834832b701
Manoel Vilela <manoel.neto@neoway.com.br>
2024-03-24 00:09:11 -0300

    Add tests/test_datasetsp.py file and fix tests pdm commands

---
+++ test.py
@@ -0,0 +1,2 @@
+from nlp_tir import datasets
+print(datasets.get_dataset_reuters_50_50())
---
+++ tests/test_datasets.py
@@ -0,0 +1,16 @@
+import pytest
+
+from nlp_tir import datasets
+
+
+@pytest.fixture
+def mock_datasets_folder(monkeypatch):
+    monkeypatch.setattr(datasets, "datasets_dir", "/tmp")
+
+
+def test_get_dataset_reuters_50_50(mock_datasets_folder):
+    r50_50 = datasets.get_dataset_reuters_50_50()
+    df_train, df_test = r50_50["train"], r50_50["test"]
+
+    assert len(df_train) == 2500
+    assert len(df_test) == 2500
